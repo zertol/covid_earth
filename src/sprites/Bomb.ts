c7b9a1f85cd488e0a3e18bd7fca6fa67eb0fd0e9
zertol <eliekad6@gmail.com>
2020-04-23 17:17:31 +0300

    a

---
+++ src/sprites/Bomb.ts
@@ -0,0 +1,25 @@
+import { CST } from '../CST';
+
+export default class Bomb extends Phaser.GameObjects.Sprite {
+
+    private animation: string;
+
+    constructor(scene: Phaser.Scene, x: number, y: number, name: string, animation: string, depth: number) {
+        super(scene, x, y, name);
+
+        this.animation = animation;
+        this.depth = depth;
+        this.name = name;
+
+        scene.add.existing(this);
+        this.play(this.animation);
+        scene.physics.world.enableBody(this);
+
+    }
+
+    update() {
+        //@ts-ignore
+        this.body.velocity.y = 300;
+        this.rotation += 0.08;
+    }
+}
